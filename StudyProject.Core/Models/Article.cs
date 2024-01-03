832bb5604b0661335eb88ffe1e38f30fcb840f51
Thiago <thiagomvas@gmail.com>
2024-01-02 22:05:41 -0300

    Add base models

---
+++ StudyProject.Core/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyProject.Core.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public Subject[] Subjects { get; set; } = new Subject[0];
+    }
+}
