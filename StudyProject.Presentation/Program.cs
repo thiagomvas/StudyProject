07158eae1978a45b44ad21afecab5f08e46c8130
Thiago <thiagomvas@gmail.com>
2024-01-23 11:41:13 -0300

    Attempt 2

--- StudyProject.Presentation/Program.cs
+++ StudyProject.Presentation/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using StudyProject.Application.Articles.Commands;
-using StudyProject.Infrastructure;
 using StudyProject.Presentation;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,12 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-var db = new ApplicationDbContext();
-await db.LoadData(builder.HostEnvironment.BaseAddress);
-
-ArticleCommandInvoker invoker = new(db);
-builder.Services.AddSingleton(invoker);
 
 await builder.Build().RunAsync();
 
