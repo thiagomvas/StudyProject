f083e41d0038d49579714a1da8aadd595de693fa
Thiago <thiagomvas@gmail.com>
2024-02-04 20:15:18 -0300

    Add Firebase Infrastructure DI

---
+++ StudyProject.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Firebase.Database;
+using Microsoft.Extensions.DependencyInjection;
+using StudyProject.Application.Common.Interfaces;
+
+namespace StudyProject.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            var client = new FirebaseClient("https://wikiforum-6f73d-default-rtdb.firebaseio.com/");
+
+            services.AddScoped<IDatabaseContext, FirebaseContext>(s => new FirebaseContext(client));
+        }
+    }
+}
