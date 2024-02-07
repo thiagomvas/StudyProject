4ab711a556e72ee72bb8802a46c65cb203104072
Thiago <thiagomvas@gmail.com>
2024-02-07 17:58:08 -0300

    Add theme class to hold all default colors

---
+++ StudyProject.Presentation/Theme.cs
@@ -0,0 +1,84 @@
+﻿namespace StudyProject.Presentation
+{
+	public class Theme
+	{
+		private static Theme instance;
+		public bool DarkMode = true;
+
+		private Theme()
+		{
+			// Private constructor to prevent instantiation
+		}
+
+		public static Theme Default
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new Theme();
+				}
+				return instance;
+			}
+		}
+
+		public string BackgroundColor { 
+			get
+			{
+				if(DarkMode)
+				{
+					return "#25252D";
+				}
+				else
+				{
+					return "#393646";
+				}
+			} 
+		}
+
+		public string ForegroundColor
+		{
+			get
+			{
+				if(DarkMode)
+				{
+					return "#30303C";
+				}
+				else
+				{
+					return "#4F4557";
+				}
+			} 
+		}
+
+		public string PrimaryColor
+		{
+			get
+			{
+				if(DarkMode)
+				{
+					return "#363646";
+				}
+				else
+				{
+					return "#6D5D6E";
+				}
+			} 
+		}
+
+		public string TextColor
+		{
+			get
+			{
+				if(DarkMode)
+				{
+					return "#EEEEEE";
+				}
+				else
+				{
+					return "#222222";
+				}
+			} 
+		}
+	}
+}
